6015efd0f6f694c743b56bec7d2d9a2f0cedc94b
Giorgos Eftichiou <eftichiou@hotmail.com>
2024-12-04 11:01:56 +0200

    Init

---
+++ RaceGame/Car.js
@@ -0,0 +1,45 @@
+var carX = 75;
+var carY = 75;
+var carAng = 0;
+var carSpeed = 0;
+
+const GROUNDSPEED_DECAY_MULT = 0.94;
+const DRIVE_POWER = 0.5;
+const REVERSE_POWER = 0.2;
+const TURN_RATE = 0.03;
+
+function carDraw() {
+  drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);
+}
+
+function carReset() {
+  for (let eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
+    for (let eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
+      const arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+      if (trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
+        carAng = (-90 * Math.PI) / 180;
+        trackGrid[arrayIndex] = TRACK_ROAD;
+        carX = eachCol * TRACK_W + TRACK_W / 2;
+        carY = eachRow * TRACK_H + TRACK_H / 2;
+      }
+    }
+  }
+}
+
+function carMove() {
+  carSpeed *= GROUNDSPEED_DECAY_MULT;
+  if (keyHeld_Gas) {
+    carSpeed += DRIVE_POWER;
+  }
+  if (keyHeld_Reverse) {
+    carSpeed -= REVERSE_POWER;
+  }
+  if (keyHeld_TurnLeft) {
+    carAng -= TURN_RATE;
+  }
+  if (keyHeld_TurnRight) {
+    carAng += TURN_RATE;
+  }
+  carX += Math.cos(carAng) * carSpeed;
+  carY += Math.sin(carAng) * carSpeed;
+}
---
+++ RaceGame/Main.js
@@ -0,0 +1,46 @@
+/**
+ * @type {HTMLCanvasElement}
+ */
+var canvas;
+/**
+ * @type {CanvasRenderingContext2D}
+ */
+var canvasContext;
+
+window.onload = function () {
+  canvas = document.getElementById("gameCanvas");
+  canvasContext = canvas.getContext("2d");
+
+  clearScreen();
+  colorText("Loading Images", canvas.width / 2, canvas.height / 2, "white");
+
+  loadImages();
+};
+function imageLoadingDoneSoStartGame() {
+  var framesPerSecond = 30;
+  setInterval(updateAll, 1000 / framesPerSecond);
+
+  setupInput();
+
+  carReset();
+}
+
+function moveAll() {
+  carMove();
+  carTrackHandling();
+}
+
+function clearScreen() {
+  colorRect(0, 0, canvas.width, canvas.height, "black");
+}
+
+function drawAll() {
+  // clearScreen();
+  drawTracks();
+  carDraw();
+}
+
+function updateAll() {
+  moveAll();
+  drawAll();
+}
